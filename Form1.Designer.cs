61ca1dda0c5b6498043e77cd3aaed73aecfa487f
julian aguirre <julian.aguirre09@gmail.com>
2024-04-02 21:42:04 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace pryAguirreNave
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            Alien = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            lblPuntaje = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Alien).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(388, 351);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(110, 110);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.PreviewKeyDown += pictureBox1_PreviewKeyDown;
+            // 
+            // Alien
+            // 
+            Alien.BackColor = Color.Transparent;
+            Alien.Image = Properties.Resources.alien;
+            Alien.Location = new Point(98, 59);
+            Alien.Name = "Alien";
+            Alien.Size = new Size(49, 44);
+            Alien.SizeMode = PictureBoxSizeMode.StretchImage;
+            Alien.TabIndex = 1;
+            Alien.TabStop = false;
+            Alien.Tag = "Alien";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(7, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Puntaje:";
+            // 
+            // lblPuntaje
+            // 
+            lblPuntaje.AutoSize = true;
+            lblPuntaje.Location = new Point(63, 9);
+            lblPuntaje.Name = "lblPuntaje";
+            lblPuntaje.Size = new Size(0, 15);
+            lblPuntaje.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(884, 461);
+            Controls.Add(lblPuntaje);
+            Controls.Add(label1);
+            Controls.Add(Alien);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.Transparent;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            KeyPress += Form1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Alien).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        public PictureBox Alien;
+        public System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private Label lblPuntaje;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+namespace pryAguirreNave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x;
+        void Enemigo()
+        {
+            Random random = new Random();
+            
+            if (Alien.Top >= 500)
+            {
+                x = random.Next(0, 800);
+                Alien.Location = new Point(x, 0);
+
+            }
+            else
+            {
+                Alien.Top += 10;
+            }
+        }
+        void Municion()
+        {
+            PictureBox Municion = new PictureBox();
+            Municion.SizeMode = PictureBoxSizeMode.StretchImage;
+            Municion.Size = new System.Drawing.Size(50, 50);
+            Municion.Image = Properties.Resources.fuego;
+            Municion.Tag = "Municion";
+            Municion.Left = pictureBox1.Left + 15;
+            Municion.Top = pictureBox1.Top - 30;
+            this.Controls.Add(Municion);
+            Municion.BringToFront();
+
+        }
+        void Movimiento()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "Municion")
+                {
+                    x.Top -= 10;
+                    if (x.Top < 50)
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+        int Puntos;
+        void Puntaje()
+        {
+            foreach (Control j in this.Controls)
+            {
+                foreach (Control i in this.Controls)
+                {
+                    if(j is PictureBox && j.Tag == "Municion")
+                    {
+                        if (i is PictureBox && i.Tag == "Alien")
+                        {
+                            if (j.Bounds.IntersectsWith(i.Bounds))
+                            {
+                                this.Controls.Remove(i);
+                                Enemigo();                                
+                                Puntos++;
+                                lblPuntaje.Text = Puntos.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            if (e.KeyChar == 'a') x -= 10;
+            if (e.KeyChar == 'd') x += 10;
+            if (e.KeyChar == 'w') Municion();
+            if (x >= 850) x = 0;
+            if (x <= -20) x = 800;
+            Point punto = new Point(x, y);
+            pictureBox1.Location = punto;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Enemigo();
+            Movimiento();
+            Puntaje();
+        }
+    }
+}
